371b590fd902685d411b60c765cdf2fa82654de6
Atahan <atahan012000@gmail.com>
2020-09-19 22:09:47 +0300

    Now fireworks doesnt shoot when you load the video, & improved party settings

--- IPythonDisplayFireworks/__init__.py
+++ IPythonDisplayFireworks/__init__.py
@@ -11,19 +11,31 @@ def ReadFile (filename):
         return data
 
 
-def Fireworks ():
-    """Shoot up some fireworks!"""
+def Fireworks (time=4,multiplier=1,party=False):
+    """Shoot up some fireworks!
+    
+    Keyword arguments:
+    time -- the time fireworks will last in seconds
+    multiplier -- By default the fireworks will shoot every (10 + (30 * Math.random()))/multiplier frames (60fps). Increase this to get more fireworks
+    party -- will make fireworks shoot up infinitely. Clear the cell output to stop.
+    """
     # this is so that each Firework gets their own canvas. Without this they all try to draw to the first created canvas
     randHash = random.getrandbits(128)
 
     # Put everything together and send it in one go
     htmlString = "";
     ## Canvas creation
-    htmlString += ('<script>%s</script>'%(ReadFile('paper-full.min.js'))) + "\n"
-    htmlString += ('<script>%s</script>'% (ReadFile('TweenMax.min.js'))) + "\n"
-    htmlString += ('<canvas id="canv' + str(randHash)+ '" position=absolute style="position: fixed; top: 0; left: 0; width:100%; height:100%; pointer-events: none;"></canvas>') + "\n"
+    htmlString += ('<script id="helpers1'+str(randHash)+'" class="fireworkscanvas">%s</script>'%(ReadFile('paper-full.min.js'))) + "\n"
+    htmlString += ('<script id="helpers2'+str(randHash)+'" class="fireworkscanvas">%s</script>'% (ReadFile('TweenMax.min.js'))) + "\n"
+    htmlString += ('<canvas id="fireworkscanvas'+str(randHash)+'" class="fireworkscanvas" position=absolute style="position: fixed; top: 0; left: 0; width:100%; height:100%; pointer-events: none;"></canvas>') + "\n"
     ##^^ make a canvas that occupies the whole screen and is click-through
     
+    if(party):
+        partyMode = "true";
+    else:
+        partyMode = "false";
     ## Drawing the fireworks
-    htmlString += ('<script type="text/paperscript" canvas="canv%s">%s</script>'% (randHash, ReadFile('Fireworks.js')))
+    htmlString += ('<script type="text/paperscript" canvas="fireworkscanvas%s" id="fireworkscanvascode%s" class="fireworkscanvas">'%(randHash, randHash)+\
+                        'canvashash="%s";time=%s;multiplier=%s;partyMode=%s; %s</script>'% \
+                        (randHash, time, multiplier, partyMode, ReadFile('Fireworks.js')))
     display(HTML(htmlString))
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="IPythonDisplayFireworks", # Replace with your own username
-    version="0.0.2",
+    version="0.0.4",
     author="Atahan Ozturk",
     author_email="atahan012000@gmail.com",
     description="Fireworks using IPython Display",
